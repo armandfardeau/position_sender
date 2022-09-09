20b8514fb3b58ff9a0fd5b9721776ecb8cf951be
armandfardeau <fardeauarmand@gmail.com>
2022-09-09 15:34:48 +0200

    Make number an array of numbers

--- src/gps_sender.cpp
+++ src/gps_sender.cpp
@@ -5,7 +5,6 @@
 #define FONA_RX 2
 #define FONA_TX 3
 #define FONA_RST 4
-uint16_t adc;
 uint16_t vbat;
 
 SoftwareSerial fonaSS = SoftwareSerial(FONA_TX, FONA_RX);
@@ -14,19 +13,22 @@ Adafruit_FONA fona = Adafruit_FONA(FONA_RST);
 
 char pin[5] = {1, 2, 3, 4};
 
+
+String contact_numbers[] = {"0033650065421", "0033612686923"};
+
 // Send sms interval (1 hour here)
 long interval = 3600000;
 
 void setup() {
-    while (! Serial);
+    while (!Serial);
 
     Serial.begin(115200);
     Serial.println(F("Initializing FONA... (May take a few seconds)"));
 
     fonaSerial->begin(4800);
-    if (! fona.begin(*fonaSerial)) {
+    if (!fona.begin(*fonaSerial)) {
         Serial.println(F("Couldn't find FONA"));
-        while (1);
+        while (true);
     }
     Serial.println(F("FONA is OK"));
     Serial.println(F("Enabling GPS..."));
@@ -37,9 +39,6 @@ void setup() {
 
     Serial.print(F("Unlocking SIM card: "));
     Serial.println(fona.unlockSIM(pin) ? F("OK") : F("FAIL"));
-
-    Serial.print(F("VBat = ")); Serial.print(vbat); Serial.println(F(" mV"));
-    Serial.print(F("VPct = ")); Serial.print(vbat); Serial.println(F("%"));
 
     // Waiting for network fix
     uint8_t n = fona.getNetworkStatus();
@@ -59,29 +58,55 @@ void setup() {
         Serial.print(F("GPS status: No fix. Trying again..."));
     }
     Serial.println(F("GPS status: Fix"));
+
+    // Displays contact info
+    Serial.println(F("Contacts info:"));
+    for (int i = 0; i < sizeof(contact_numbers) / sizeof(contact_numbers[0]); i++) {
+        Serial.print("Contact number " + String(i) + ": ");
+        Serial.println(contact_numbers[i]);
+    }
 }
 
 void loop() {
-    float latitude, longitude, speed_kph, heading, speed_mph, altitude;
-    boolean gps_success = fona.getGPS(&latitude, &longitude, &speed_kph, &heading, &altitude);
+    float latitude, longitude;
+    boolean gps_success = fona.getGPS(&latitude, &longitude);
 
     if (gps_success) {
-
         String lat = String(latitude, 6);
         String lon = String(longitude, 6);
 
+        // Display the GPS data
         Serial.println("Latitude: " + lat);
         Serial.println("Longitude: " + lon);
 
+        // Display hardware info
+        fona.getBattVoltage(&vbat);
+        fona.getBattPercent(&vbat);
+
+        // Display the battery info
+        Serial.print(F("VBat = "));
+        Serial.print(vbat);
+        Serial.println(F(" mV"));
+
+        Serial.print(F("VPct = "));
+        Serial.print(vbat);
+        Serial.println(F("%"));
+
         String message = "https://www.google.com/maps/search/?api=1&query=" + lat + "%2C" + lon;
-        String sendTo = "0033650065421";
 
-        Serial.println("Sending SMS to " + sendTo + " with content '" + message + "'");
+        //iterating through the contact_numbers array
+        for (int i = 0; i < sizeof(contact_numbers) / sizeof(contact_numbers[0]); i++) {
+            Serial.println("Sending SMS to: " + contact_numbers[i] + " with content '" + message + "'");
+            boolean sms_status = fona.sendSMS(
+                    const_cast<char *>(contact_numbers[i].c_str()),
+                    const_cast<char *>(message.c_str())
+            );
 
-        if (!fona.sendSMS(const_cast<char*>(sendTo.c_str()), const_cast<char*>(message.c_str()))) {
-            Serial.println(F("Failed"));
-        } else {
-            Serial.println(F("Sent!"));
+            if (!sms_status) {
+                Serial.println(F("Failed"));
+            } else {
+                Serial.println(F("Sent!"));
+            }
         }
 
         delay(interval);
